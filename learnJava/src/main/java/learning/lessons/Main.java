5e9b1fd55467a2ababad80744f2e25ef37234b38
Elkirillo <kirill.gora@mail.ru>
2023-09-17 22:53:53 +0300

    Some exercises 1

---
+++ learnJava/src/main/java/learning/lessons/Main.java
@@ -0,0 +1,12 @@
+package main.java.learning.lessons;
+
+import main.java.learning.lessons.test_list.ListExample;
+
+public class Main {
+    public static void main(String[] args) {
+        ListExample listExample = new ListExample();
+        listExample.addList();
+        listExample.getList();
+        System.out.println("Hello world!");
+    }
+}
---
+++ learnJava/src/main/java/learning/lessons/test_list/ListExample.java
@@ -0,0 +1,33 @@
+package main.java.learning.lessons.test_list;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class ListExample {
+    private List<String> listcont = new ArrayList<>();
+    public void addList () {
+        listcont.add("my name is");
+        listcont.add("Gribovik");
+        listcont.add(1, "Not is");
+        listcont.set(0, "your name");
+        List<String> newList = getList();
+        printList(newList);
+    }
+
+    public List<String> getList() {
+        List<String> list = new ArrayList<>();
+        for (String listcon : listcont) {
+            list.add(listcon);
+        }
+        return list;
+    }
+
+    public void printList(List<String> lists) {
+        for (String list : lists) {
+            System.out.println(list);
+        }
+    }
+
+
+}
