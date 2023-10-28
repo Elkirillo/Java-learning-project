37da035a2359083b53663f204c882b77ff3d77d6
Elkirillo <kirill.gora@mail.ru>
2023-10-29 00:19:23 +0300

    Add jackson and spring

--- learnJava/src/main/java/learning/lessons/Main.java
+++ learnJava/src/main/java/learning/lessons/Main.java
@@ -1,9 +1,16 @@
 package main.java.learning.lessons;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import main.java.learning.lessons.test_list.ListExample;
+
+import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        ResultPOJO resultPOJO = new ResultPOJO("123", "Valaev");
         ListExample listExample = new ListExample();
         listExample.addList();
         listExample.getList();
